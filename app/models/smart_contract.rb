d7d6b5b9846ba066029b8d406e034649f40c045a
Steven Chang <stevenchang5000@protonmail.com>
2023-09-21 14:47:52 +1000

    SmartContract: validations

--- app/models/smart_contract.rb
+++ app/models/smart_contract.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class SmartContract < ApplicationRecord
+	# === ACTIVE STORAGE ===
+	has_one_attached :abi
+	has_one_attached :contract
+	has_one_attached :wasm
+
+	# === VALIDATIONS ===
+	validates :third_party_identifier, uniqueness: { case_sensitive: false }
+	# ACTIVE STORAGE
+	validates :abi, attached: true, content_type: :json, size: { less_than: 500.kilobyte }
+	# octet_stream is the content_type of a .contract file
+	validates :contract, content_type: :octet_stream, size: { less_than: 500.kilobyte }
+	validates :wasm, content_type: :wasm, size: { less_than: 500.kilobyte }
 end
--- spec/models/smart_contract_spec.rb
+++ spec/models/smart_contract_spec.rb
@@ -3,5 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe SmartContract do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'VALIDATIONS' do
+    before { create(:smart_contract) }
+
+    it { should validate_uniqueness_of(:third_party_identifier).case_insensitive }
+
+    describe 'active storage' do
+      it { should validate_attached_of(:abi) }
+      it { should validate_content_type_of(:abi).allowing('application/json') }
+      it { should validate_size_of(:abi).less_than(500.kilobytes) }
+      it { should validate_content_type_of(:contract).allowing('application/octet-stream') }
+      it { should validate_size_of(:contract).less_than(500.kilobytes) }
+      it { should validate_content_type_of(:wasm).allowing('application/wasm') }
+      it { should validate_size_of(:wasm).less_than(500.kilobytes) }
+    end
+  end
 end
