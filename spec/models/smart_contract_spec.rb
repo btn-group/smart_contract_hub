8a6672e3260b17221f960e0c9632118efda4c269
Steven Chang <stevenchang5000@protonmail.com>
2023-09-21 12:27:53 +1000

    Clean

--- spec/models/smart_contract_spec.rb
+++ spec/models/smart_contract_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe SmartContract, type: :model do
+RSpec.describe SmartContract do
   pending "add some examples to (or delete) #{__FILE__}"
 end
