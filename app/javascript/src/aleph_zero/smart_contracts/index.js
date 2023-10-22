fe1e042039027cd49efde4b8085ab33368c4df0f
Steven Chang <stevenchang5000@protonmail.com>
2023-10-22 14:55:41 +1000

    datatable processing

--- app/javascript/src/aleph_zero/smart_contracts/index.js
+++ app/javascript/src/aleph_zero/smart_contracts/index.js
@@ -1,62 +1,62 @@
-import DataTable from "datatables.net-bs5";
+import { ALEPH_ZERO } from "../helpers";
 
 const SMART_CONTRACTS_INDEX = {
   datatable: undefined,
   queryCount: 0,
   init: async () => {
     SMART_CONTRACTS_INDEX.addListeners();
-    SMART_CONTRACTS_INDEX.datatable = new DataTable("#smart-contracts-table", {
-      columns: [
-        {
-          data: "id",
-          title: "#",
-        },
-        {
-          data: "address",
-          title: "Smart Contract",
-        },
-        {
-          data: "caller",
-          title: "Caller",
-        },
-        {
-          data: "azeroId",
-          title: "AZERO.ID",
-        },
-        {
-          data: "group.name",
-          title: "Group",
-        },
-      ],
-      ordering: false,
-      paging: false,
-      processing: true,
-      bInfo: false,
-      searching: false,
-    });
+    if (!SMART_CONTRACTS_INDEX.datatable) {
+      SMART_CONTRACTS_INDEX.datatable = new DataTable("#smart-contracts-table", {
+        columns: [
+          {
+            data: "id",
+            title: "#",
+          },
+          {
+            data: "address",
+            title: "Smart Contract",
+          },
+          {
+            data: "caller",
+            title: "Caller",
+          },
+          {
+            data: "azeroId",
+            title: "AZERO.ID",
+          },
+          {
+            data: "group.name",
+            title: "Group",
+          },
+        ],
+        ordering: false,
+        paging: false,
+        processing: true,
+        bInfo: false,
+        searching: false,
+      });
+    }
+    document.datatable = SMART_CONTRACTS_INDEX.datatable;
   },
   addListeners: () => {
     $("#search-input").on("input", async (_evt) => {
-      console.log(555);
       // 1. Set to loading and clear table
       SMART_CONTRACTS_INDEX.datatable.clear();
       SMART_CONTRACTS_INDEX.datatable.processing(true);
 
       // 2. Increase query count
       SMART_CONTRACTS_INDEX.queryCount += 1;
-      let currentQueryCount = ALEPH_ZERO.SMART_CONTRACTS_INDEX;
+      let currentQueryCount = SMART_CONTRACTS_INDEX.queryCount;
 
       // 3. Search
       let search = $("#search-input").val();
       let smartContracts = [];
       try {
         if (search.length) {
-          console.log(333);
-          console.log(document.delay);
-          await document.delay(5_000);
+          await document.delay(500);
           let response = await $.ajax({
             type: "post",
-            url: "https://squid.subsquid.io/smart-contract-hub/graphql",
+            url: ALEPH_ZERO.subsquid.url,
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify({
               query: `query MyQuery {
@@ -86,6 +86,7 @@ const SMART_CONTRACTS_INDEX = {
       } finally {
         if (currentQueryCount == SMART_CONTRACTS_INDEX.queryCount) {
           SMART_CONTRACTS_INDEX.datatable.rows.add(smartContracts);
+          $('#smart-contracts-table').width("100%");
           SMART_CONTRACTS_INDEX.datatable.columns.adjust().draw();
           SMART_CONTRACTS_INDEX.datatable.processing(false);
         }
@@ -97,7 +98,6 @@ const SMART_CONTRACTS_INDEX = {
 // Even with turbo, init is called every time as listeners need to be replaced
 $(document).on("turbo:load", function () {
   if ($("#smart-contracts-index").length) {
-    console.log(554)
     SMART_CONTRACTS_INDEX.init();
   }
 });
