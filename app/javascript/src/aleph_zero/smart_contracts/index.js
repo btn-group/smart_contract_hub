c3fe72094f186e5ee90c827493094704c17bcc8f
Steven Chang <stevenchang5000@protonmail.com>
2023-10-25 20:03:21 +1000

    Index datatable subtables init

--- app/javascript/src/aleph_zero/smart_contracts/index.js
+++ app/javascript/src/aleph_zero/smart_contracts/index.js
@@ -16,6 +16,12 @@ const SMART_CONTRACTS_INDEX = {
         {
           columns: [
             {
+              className: "dt-control",
+              orderable: false,
+              data: null,
+              defaultContent: "",
+            },
+            {
               data: "id",
               title: "#",
             },
@@ -28,17 +34,10 @@ const SMART_CONTRACTS_INDEX = {
               title: "Caller",
             },
             {
-              data: "azeroId",
-              title: "AZERO.ID",
-            },
-            {
-              data: "group.name",
-              title: "Group",
-            },
-            {
               searchable: false,
               className: "text-end",
               defaultContent: "",
+              title: "Actions",
               fnCreatedCell: function (nTd, _sData, oData, _iRow) {
                 if (
                   ALEPH_ZERO.account &&
@@ -79,6 +78,22 @@ const SMART_CONTRACTS_INDEX = {
   addListeners: () => {
     $(document).on("aleph_zero_account_selected", async () => {
       $("#search-input").trigger("input");
+    });
+
+    // Add event listener for opening and closing details
+    SMART_CONTRACTS_INDEX.datatable.on("click", "td.dt-control", function (e) {
+      let tr = e.target.closest("tr");
+      let row = SMART_CONTRACTS_INDEX.datatable.row(tr);
+
+      if (row.child.isShown()) {
+        // This row is already open - close it
+        row.child.hide();
+      } else {
+        // Open this row
+        row
+          .child(SMART_CONTRACTS_INDEX.datatableChildRowFormat(row.data()))
+          .show();
+      }
     });
 
     $("#search-input").on("input", async (_evt) => {
@@ -134,6 +149,66 @@ const SMART_CONTRACTS_INDEX = {
         }
       }
     });
+  },
+  chainToString: (index) => {
+    let mapping = { 0: "Production", 1: "Testnet" };
+    return mapping[index];
+  },
+  // pub struct SmartContract {
+  //     id: u32,
+  //     smart_contract_address: AccountId,
+  //     chain: u8,
+  //     caller: AccountId,
+  //     enabled: bool,
+  //     azero_id: String,
+  //     abi_url: String,
+  //     contract_url: Option<String>,
+  //     wasm_url: Option<String>,
+  //     audit_url: Option<String>,
+  //     group_id: Option<u32>,
+  //     project_name: Option<String>,
+  //     project_website: Option<String>,
+  //     github: Option<String>,
+  // }
+  datatableChildRowFormat: (d) => {
+    let html =
+      "<div class='table-responsive'><table class='table fs-6 align-middle mb-0'><tbody>";
+    // id
+    html += `<tr><th>#</th><td>${d.id}</td></tr>`;
+    // smart_contract_address
+    html += `<tr><th>Address</th><td>${d.smartContractAddress}</td></tr>`;
+    // chain
+    html += `<tr><th>Chain</th><td>${SMART_CONTRACTS_INDEX.chainToString(
+      d.chain
+    )}</td></tr>`;
+    // caller
+    html += `<tr><th>Caller</th><td>${d.caller}</td></tr>`;
+    // enabled
+    html += `<tr><th>Enabled</th><td>${d.enabled}</td></tr>`;
+    // azero_id
+    html += `<tr><th>AZERO.ID</th><td>${d.azeroId}</td></tr>`;
+    // abi_url
+    html += `<tr><th>ABI URL</th><td>${d.abiUrl}</td></tr>`;
+    // contract_url
+    html += `<tr><th>Contract URL</th><td>${d.contractUrl}</td></tr>`;
+    // wasm_url
+    html += `<tr><th>WASM URL</th><td>${d.wasmUrl}</td></tr>`;
+    // audit_url
+    html += `<tr><th>Audit URL</th><td>${d.auditUrl}</td></tr>`;
+    // group_id
+    let groupName = "";
+    if (d.group) {
+      groupName = d.group.name;
+    }
+    html += `<tr><th>Group</th><td>${groupName}</td></tr>`;
+    // project_name
+    html += `<tr><th>Project Name</th><td>${d.projectName}</td></tr>`;
+    // project_website
+    html += `<tr><th>Project Website</th><td>${d.projectWebsite}</td></tr>`;
+    // github
+    html += `<tr><th>Github</th><td>${d.github}</td></tr>`;
+    html += "</tbody></table></div>";
+    return html;
   },
 };
 
